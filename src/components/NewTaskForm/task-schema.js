88ebc0bee1ea55abb8c8dc11aa332f21ffad32b1
Joel <joelmarfas@gmail.com>
2021-09-27 11:38:52 +0200

    deleted

--- src/components/NewTaskForm/task-schema.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup";
-
-const taskSchema = Yup.object().shape({
-  text: Yup.string().required("The title is too required").min(4, 'The title is too short!').max(50, 'The title is too long!'),
-  done: Yup.bool()
-});
-
-export default taskSchema;
