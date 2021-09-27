8408a797c49a8ac91ded96c7cc371857feec3388
Paul Molano <paulmolanoo@gmail.com>
2021-09-27 11:15:18 +0200

    newVersion

---
+++ reactjs-todo-list-main/src/components/index.js
@@ -0,0 +1,15 @@
+import Footer from './Footer';
+import Main from './Main';
+import NewTaskForm from './NewTaskForm';
+import Task from './Task';
+import TasksList from './TasksList';
+import Toggle from './Toggle';
+
+export {
+  Footer,
+  Main,
+  NewTaskForm,
+  Task,
+  TasksList,
+  Toggle,
+}
